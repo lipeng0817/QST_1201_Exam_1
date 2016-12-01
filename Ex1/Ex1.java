e988443689b4ab989a80b270019a20277026ac8c
lipeng0817 <1394831919@qq.com>
2016-12-01 16:17:24 +0800

    Create Ex1.java

---
+++ Ex1/Ex1.java
@@ -0,0 +1,61 @@
+package Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QSTLinkList{
+		public static class Node {
+			public int value;
+			public Node next = null;
+			Node(int value) {
+				this.value = value;
+			}
+			Node(int value, Node next) {
+				this.value = value;
+				this.next = next;
+			}
+			public void setValue(int value) {
+				this.value = value;
+			}
+		}
+
+		private static void printLinkList(Node head) {
+			Node p = head;
+			List<Object> list = new ArrayList<Object>();
+			String s ="";
+			while(p!=null){
+				list.add(p.value);
+				list.add("->");
+				p=p.next;
+			}
+			for(int i = 0;i<list.size()-1;i++){
+				System.out.print(list.get(i));
+			}
+		}
+		
+		public static void main(String[] args) {
+			int[] arr = { 1, 3, 5, 7, 2, 4 };
+			Node head = createLinkList(arr);
+			printLinkList(head);
+		}
+		
+		private static Node createLinkList(int[] arr) {
+			// TODO Auto-generated method stub
+			Node[] linkArr = new Node[arr.length];
+			for (int i = 0; i < arr.length; i++) {
+				linkArr[i] = new Node(arr[i]);
+			}
+			for (int i = 0; i < arr.length; i++) {
+				linkArr[i].setValue(arr[i]);
+				if (i == arr.length - 1) {
+					linkArr[i].next = null;
+				}
+				else {
+					linkArr[i].next = linkArr[i + 1];
+				}
+			}
+			return linkArr[0];
+		}
+	}
+
+
